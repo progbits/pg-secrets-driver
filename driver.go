af8e6937a5d791f6ed54536aa60c039de9dd2a0f
progbits <53880655+progbits@users.noreply.github.com>
2022-06-29 21:22:44 +0100

    Basic secrets aware database connector

--- driver.go
+++ driver.go
@@ -1,3 +1,60 @@
-package driver
+package connector
 
-type PgSecretsDriver struct{}
+import (
+	"context"
+	"database/sql/driver"
+	"github.com/lib/pq"
+	_ "github.com/lib/pq"
+	"sync"
+)
+
+// CredentialsProvider implementations return a valid Postgres data source
+// name.
+type CredentialsProvider interface {
+	GetDataSourceName() (string, error)
+}
+
+// PgSecretsConnector implements the driver.Connector interface.
+type PgSecretsConnector struct {
+	mutex    sync.Mutex
+	driver   driver.Driver
+	provider CredentialsProvider
+}
+
+func NewPgSecretsConnector(provider CredentialsProvider) *PgSecretsConnector {
+	return &PgSecretsConnector{
+		driver:   pq.Driver{},
+		provider: provider,
+	}
+}
+
+func (c *PgSecretsConnector) Connect(ctx context.Context) (driver.Conn, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	dataSourceName, err := c.provider.GetDataSourceName()
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := c.driver.Open(dataSourceName)
+	if err != nil {
+		if e, ok := err.(*pq.Error); ok {
+			if e.Code == "28P01" {
+				// Invalid password.
+				dataSourceName, err := c.provider.GetDataSourceName()
+				if err != nil {
+					return nil, err
+				}
+				return c.driver.Open(dataSourceName)
+			}
+		}
+		return nil, err
+	}
+	return conn, err
+}
+
+// Driver returns the underlying driver of the connector.
+func (c *PgSecretsConnector) Driver() driver.Driver {
+	return c.driver
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/progbits/pg-secrets-driver
 
 go 1.18
+
+require github.com/lib/pq v1.10.6
